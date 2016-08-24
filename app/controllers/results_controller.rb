67947ef0d8ad98b284e3ed34affa2d64e060db0b
Azzapop <a.b.hook@live.com.au>
2016-08-24 17:33:46 +1000

    blah

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -7,7 +7,7 @@ class ResultsController < ApplicationController
     results = JSON.parse(Hash.from_xml(params["dr"]).to_json)
     puts JSON.pretty_unparse(results)
     puts "###############################################"
-    resultIndex results["quizReport"]["questions"]["yesNoQuestion"]["userAnswerIndex"]
+    resultIndex = results["quizReport"]["questions"]["yesNoQuestion"]["userAnswerIndex"]
     puts results["quizReport"]["questions"]["yesNoQuestion"]["answer"][resultIndex]
   end
 
